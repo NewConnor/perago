f3844058fd9f6747f66c9d22919f9e68366f500f
NewConnor <connorpriet@gmail.com>
2025-05-30 14:19:41 +0900

    bet u know

--- interpreter.py
+++ interpreter.py
@@ -1,2 +1,27 @@
 import sys
 
+'''
+    IDENT
+    NUM
+    STR
+    ASSIGN
+    PLUS
+    MINUS
+    MULTIP
+    DIVIDE
+    MODUL
+    WSPACE
+    ERROR
+'''
+
+class Lexer:
+    def __init__(self):
+        self.serial = ""
+
+    def SCAN_IDENT(self, string=""):
+        if string[0].isalpha() or string[0] == '_':
+            for i in range(len(string)):
+                if string[i].isalnum or string[i] == '_' or string[i] == '.':
+                    continue
+            else:
+                return (string[0:i], "IDENT")
